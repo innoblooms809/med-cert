31d5f21a2d071a329e5b45af38c768ee5f2786d5
ashishinnoblooms <ashish.maurya@innoblooms.com>
2025-12-04 12:30:31 +0530

    Removed console logs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import './global.css';
+import "./global.css";
 import { ClientProvider } from "@/components/ClientProvider";
 
 const geistSans = Geist({
@@ -26,9 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-         <ClientProvider>
-          {children}
-         </ClientProvider>
+        <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
   );
